69be7efb4c0a12eab850feb42b19e998fd2cfaf9
Gwendal <47559821+GwendalRaoul@users.noreply.github.com>
2020-09-03 21:39:05 +0200

    Update firstboot.sh

--- firstboot.sh
+++ firstboot.sh
@@ -47,3 +47,9 @@ mv com.wirepas.sink.conf /etc/dbus-1/system.d/
 systemctl enable wirepasSink1.service
 systemctl enable wirepasTransport.service
 systemctl enable wirepasConfigurator.service
+
+# Rename the script once successful execution to stop execution
+mv /home/pi/wirepas/firstboot.sh /home/pi/wirepas/firstboot.sh.done 
+
+# Reboot so that other service will be started too
+reboot
