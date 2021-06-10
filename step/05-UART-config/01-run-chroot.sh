480cfda99da929ca182ad86293f7489d8bd512cc
Gwendal Raoul <gwendal.raoul@wirepas.com>
2021-06-10 17:23:43 +0200

    Disable uart

--- step/05-UART-config/01-run-chroot.sh
+++ step/05-UART-config/01-run-chroot.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-echo "Disable BT"
-dtoverlay disable-bt
+echo "Do modification for rpi hat (to take control of /dev/ttyAMA0 instead of onboard bluetooth chip"
+if grep -Fq "console=serial0,115200 console=tty1" /boot/cmdline.txt
+then
+  sed -i 's/console=serial0,115200 console=tty1 //g' /boot/cmdline.txt
+else
+  echo "Cannot update cmdline.txt"
+  exit 1
+fi
 
+if test -f /boot/config.txt
+then
+  echo "dtoverlay=pi3-disable-bt" >> /boot/config.txt
+  echo "dtoverlay=pi3-miniuart-bt" >> /boot/config.txt
+else
+  echo "Cannot update config.txt"
+  exit 1
+fi
 
 echo "disable related services"
 systemctl disable hciuart.service
