b5e7a90f0b1b8edcb12746b8204f93ef039042d1
Gwendal Raoul <gwendal.raoul@wirepas.com>
2021-06-14 23:12:24 +0200

    Fix access right
    Test docker

--- step/06-gateway-service/01-run.sh
+++ step/06-gateway-service/01-run.sh
@@ -7,12 +7,19 @@ echo "Add docker compose to home folder"
 install -m 755 files/docker-compose.yml	"${ROOTFS_DIR}/home/${FIRST_USER_NAME}/"
 
 echo "Add systemd service to start Gateway at boot time"
-install -m 755 files/wirepasGatewayUpdate.service	"${ROOTFS_DIR}/etc/systemd/system/"
+install -m 644 files/wirepasGatewayUpdate.service	"${ROOTFS_DIR}/etc/systemd/system/"
 
 echo "Execute install script"
 on_chroot << EOF
 systemctl enable wirepasGatewayUpdate.service
 
+# TODO: to be removed
+docker info
+
+docker-compose --version
+
+echo "Checkout latest tag"
+
 # To speed up first build, checkout latest tag
-docker-compose -f /home/${FIRST_USER_NAME}/docker-compose.yml pull
+GATEWAY_TAG=latest docker-compose -f /home/${FIRST_USER_NAME}/docker-compose.yml pull
 EOF
