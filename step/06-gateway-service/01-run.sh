5f63b7f720aa2a3e8bf677810147d61021ceed3d
Gwendal Raoul <gwendal.raoul@wirepas.com>
2021-06-14 08:55:15 +0200

    Rename and add log

--- step/06-gateway-service/01-run.sh
+++ step/06-gateway-service/01-run.sh
@@ -1,11 +1,15 @@
 #!/bin/bash -e
 
+echo "Create wirepas folder on boot"
 install -v -d  "${ROOTFS_DIR}/boot/wirepas"
 
+echo "Add docker compose to home folder"
 install -m 755 files/docker-compose.yml	"${ROOTFS_DIR}/home/${FIRST_USER_NAME}/"
 
+echo "Add systemd service to start Gateway at boot time"
 install -m 755 files/wirepasGatewayUpdate.service	"${ROOTFS_DIR}/etc/systemd/system/"
 
+echo "Execute install script"
 on_chroot << EOF
 systemctl enable wirepasGatewayUpdate.service
 EOF
