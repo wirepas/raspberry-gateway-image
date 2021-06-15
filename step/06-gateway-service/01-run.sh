0bb4f7c5ae18938d95d45af65c8f3059b6bfcd4c
Gwendal Raoul <gwendal.raoul@wirepas.com>
2021-06-15 09:58:16 +0200

    Preload latest tag without docker daemon

--- step/06-gateway-service/01-run.sh
+++ step/06-gateway-service/01-run.sh
@@ -9,20 +9,17 @@ install -m 755 files/docker-compose.yml	"${ROOTFS_DIR}/home/${FIRST_USER_NAME}/"
 echo "Add systemd service to start Gateway at boot time"
 install -m 644 files/wirepasGatewayUpdate.service	"${ROOTFS_DIR}/etc/systemd/system/"
 
+echo "Add script to preload images without docker installed"
+install -m 755 files/download-frozen-image-v2.sh  "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/"
+
 echo "Execute install script"
 on_chroot << EOF
 systemctl enable wirepasGatewayUpdate.service
 
-# Start daemon manually to be able to checkout images
-dockerd
+cd /home/${FIRST_USER_NAME}
+# Download all images to speed up first boot
+./download-frozen-image-v2.sh transport_service wirepas/gateway_transport_service:latest
+./download-frozen-image-v2.sh sink_service wirepas/gateway_sink_service:latest
+./download-frozen-image-v2.sh dbus_service wirepas/gateway_dbus_service:latest
 
-# TODO: to be removed
-docker info
-
-docker-compose --version
-
-echo "Checkout latest tag"
-
-# To speed up first build, checkout latest tag
-GATEWAY_TAG=latest docker-compose -f /home/${FIRST_USER_NAME}/docker-compose.yml pull
 EOF
