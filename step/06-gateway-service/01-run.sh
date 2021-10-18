927d622a6dbf299d1a4be1cf786100dc5e621944
Gwendal Raoul <gwendal.raoul@wirepas.com>
2021-10-18 11:05:33 +0200

    WS: support ws connection on broker
    Add a default transport that publish on local (in addition with previous one)

--- step/06-gateway-service/01-run.sh
+++ step/06-gateway-service/01-run.sh
@@ -15,6 +15,9 @@ install -m 644 files/wirepasSinkConfigurator.service	"${ROOTFS_DIR}/etc/systemd/
 echo "Add script to preload images without docker installed"
 install -m 755 files/download-frozen-image-v2.sh  "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/"
 
+echo "Add mosquitto config to accept ws connection"
+install -m 644 files/config_ws_tcp.conf	"${ROOTFS_DIR}/etc/mosquitto/conf.d/"
+
 echo "Execute install script"
 on_chroot << EOF
 systemctl enable wirepasGatewayUpdate.service
