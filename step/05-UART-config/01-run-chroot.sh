0e4acde738e3edfda3a92256d8ae41ace398a076
Şan Güneş <180301198+sgunes-wirepas@users.noreply.github.com>
2024-09-10 16:16:38 +0300

    Update 05-UART-config for bookworm
    config.txt was moved from /boot to /boot/firmware

--- step/05-UART-config/01-run-chroot.sh
+++ step/05-UART-config/01-run-chroot.sh
@@ -5,10 +5,10 @@ echo "Do modification for rpi hat (to take control of /dev/ttyAMA0 instead of on
 raspi-config nonint do_serial_hw 0
 raspi-config nonint do_serial_cons 1
 
-if test -f /boot/config.txt
+if test -f /boot/firmware/config.txt
 then
-  echo "dtoverlay=pi3-disable-bt" >> /boot/config.txt
-  echo "dtoverlay=pi3-miniuart-bt" >> /boot/config.txt
+  echo "dtoverlay=pi3-disable-bt" >> /boot/firmware/config.txt
+  echo "dtoverlay=pi3-miniuart-bt" >> /boot/firmware/config.txt
 else
   echo "Cannot update config.txt"
   exit 1
